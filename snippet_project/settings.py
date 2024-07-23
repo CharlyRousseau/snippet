05ef79804952992e81c8b02ecbe05c046ee01b6f
Charly <44084502+CharlyRousseau@users.noreply.github.com>
2024-07-23 15:51:59 +0200

    add CSRF_TRUSTED_ORIGINS (#11)
    Co-authored-by: CharlyRousseau <charly@mbp-de-charly.home>

--- snippet_project/settings.py
+++ snippet_project/settings.py
@@ -22,6 +22,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["snippet.playground-charly.fr", "127.0.0.1", "localhost"]
 
+CSRF_TRUSTED_ORIGINS = ["https://snippet.playground-charly.fr"]
 
 # Application definition
 
