7301b26d33baef733accb1c6c6046e6bbd53c7d1
flhuissierEZV <flhuissier@easyvista.com>
2024-07-25 10:44:01 +0200

    reset password

--- snippet/urls.py
+++ snippet/urls.py
@@ -19,6 +19,10 @@ urlpatterns = [
     path('like/<int:snippet_id>/', views.like_snippet, name='like_snippet'),
     path('is_liked/<int:snippet_id>', views.is_snippet_liked, name='is_snippet_liked'),
     path('snippets/generation-error/', views.generation_error, name='generation_error'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 # 4XX/5XX Handling
