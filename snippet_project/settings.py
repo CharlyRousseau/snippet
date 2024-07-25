7c0da1196dc2f431e445560cbb068d3d2280d4ba
Snowsdy <67746810+Snowsdy@users.noreply.github.com>
2024-07-25 12:16:58 +0200

    🐛 Adding override for env (#13)
    * 🐛 Adding override for env
    
    * 🐛

--- snippet_project/settings.py
+++ snippet_project/settings.py
@@ -28,7 +28,7 @@ CSRF_TRUSTED_ORIGINS = ["https://snippet.playground-charly.fr"]
 # Application definition
 
 
-load_dotenv()
+load_dotenv(override=True)
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
